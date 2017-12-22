b3fed7115eaa8d08ed536de96ad51f79d55a72f0
MAF <mafphp@gmail.com>
2017-12-23 02:41:08 +0330

    added view engine  961001

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ let lib = require('./helpers/lib');
 
 
 /*** Request Body Config ****/
-// lib.app.use(lib.morgan(':method :url :status :res[content-length] - :response-time ms'));
-let accessLogStream = lib.fs.createWriteStream(lib.path.join(__dirname, 'access.log'), {flags: 'a'});
-lib.app.use(lib.morgan('combined', {stream: accessLogStream}));
+lib.app.use(lib.morgan('combined', {
+	skip: (req, res) => {
+		return res.statusCode < 400;
+	},
+	stream: lib.fs.createWriteStream(lib.path.join(__dirname, 'access.log'), {flags: 'a'}),
+}));
 lib.app.set('port', lib.port);
+lib.app.set('view engine', 'jade');
 lib.app.use(lib.bodyParser.json());
 lib.app.use(lib.bodyParser.urlencoded({extended: false}));
+lib.app.use(lib.express.static(lib.path.join(__dirname, 'public')));
+
 
 /***** Config Routes *****/
 let routes = lib.fs.readdirSync('./routes');
@@ -15,8 +21,17 @@ routes.forEach((route) => {
 	require('./routes/' + route)(lib.app);
 });
 
+/*** Middleware for Error Page****/
+lib.app.use(function (req, res, next) {
+	let error = new Error('Page Not Found');
+	error.status = '404';
+	res.status('404').render('error', {error});
+	next(error);
+});
+
+
 
 /***** App Serve ******/
-lib.app.listen(lib.port, (err) => {
+lib.app.listen(lib.port, () => {
 	console.log(`listen to port ${lib.port}`);
 });
--- controllers/panel/dashboard.js
+++ controllers/panel/dashboard.js
@@ -1,5 +1,6 @@
 let lib = require('../../helpers/lib');
+let namespace = 'panel/';
 /******** S E P A R A T O R*********/
 exports.index = (req, res) => {
-	console.log("dashboard logged");
+	res.render(namespace+'dashboard');
 };
